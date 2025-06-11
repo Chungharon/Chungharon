1aa9d49ee5348636469354059b701c7df4a8f76a
Chungharon <ngairaharon@gmail.com>
2025-06-11 13:41:14 +0300

    MCP Inspector

--- mcpserver/main.py
+++ mcpserver/main.py
@@ -1,19 +1,65 @@
-# server.py
 from mcp.server.fastmcp import FastMCP
 
-# Create an MCP server
-mcp = FastMCP("Demo")
+import os
+
+# create an MCP server
+mcp = FastMCP("AI Sticky Notes")
+
+NOTES_FILE = "notes.txt"
+
+def ensure_file():
+    if not os.path.exists(NOTES_FILE):
+        with open(NOTES_FILE, "w") as f:
+            f.write("")
 
 
-# Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
 
+def add_note(message: str) -> str:
 
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+    """
+    Append a new note to the sticky note file.
+
+    Args: 
+        message (str): The note content to be added.
+
+    Returns:
+        str: confirmation message indicating the note is saved.
+    """
+
+    ensure_file()
+    with open(NOTES_FILE, "a") as f:
+        f.write(message + "\n")
+    return "Note saved!"
+
+@mcp.tool()
+def read_notes() -> str:
+    """
+    Read and return all notes from the sticky note file.
+
+    Returns:
+           str: All notes as a single string separated by line breaks.
+           if no notes exists, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        content = f.read().strip()
+    return content or "No notes yet."
+
+@mcp.resource("notes://latest")
+def get_latest_note() -> str:
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        lines = f.readlines()
+    return lines[-1].strip() if lines else "No notes yet."
+
+@mcp.prompt()
+def note_summary_prompt() -> str:
+     
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        content = f.read().strip()
+    if not content:
+        return "There are no notes yet."
+    return f"Summaize the current notes: {content}"
+    
